ca2c58a30b08ffbe035d763efd8bb648711e52dc
Neeraj <nk88337@gmail.com>
2023-04-23 16:39:52 +0530

    test case

--- app/src/test/java/com/study/tsolutions/utils/ValidatorTest.kt
+++ app/src/test/java/com/study/tsolutions/utils/ValidatorTest.kt
@@ -3,15 +3,20 @@ package com.study.tsolutions.utils
 import org.junit.Assert
 import org.junit.Test
 
-class ValidatorTest {
+class ValidatorTest : Assert() {
 
     @Test
     fun emailValidator_CorrectEmail_ReturnsTrue() {
-        Assert.assertTrue(Validator.isEmailValid("name@email.com"))
+        assertTrue(Validator.isEmailValid("name@email.com"))
     }
 
     @Test
     fun emailValidator_IncorrectEmail_ReturnsFalse() {
-        Assert.assertFalse(Validator.isEmailValid("abc"))
+        assertFalse(Validator.isEmailValid("abc"))
+    }
+
+    @Test
+    fun test() {
+        assertTrue(Validator.isNumberValid("1234"))
     }
 }
